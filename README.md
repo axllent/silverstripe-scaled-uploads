f0846b31b4c607c8c320b0d0d1b5896608e596d6
axllent <axllent@gmail.com>
2013-04-14 12:19:41 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Automatically scale down uploaded images with auto-rotation for SilverStripe 3
+An extension to automatically scale down all uploaded images in SilverStripe 3. If the uploaded
+image is larger than a pre-configured size, it will be scaled down. The extension also supports
+auto-rotation of JPG images eg: portrait images taken with digital cameras or cellphones.
+
+## Requirements
+* SilverStripe 3+
+* GD support in PHP
+
+## Usage
+In your mysite/_config.php you may set different defaults, eg:
+<pre>
+ScaledUploads::$max_width = 800; // set max height to 800px
+ScaledUploads::$max_height = 600; // set max width to 600px
+ScaledUploads::$exif_rotation = false; // turn off auto-rotation
+</pre>
---
+++ code/ScaledUploads.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Automatically scale down uploaded images
+ * ========================================
+ *
+ * Extension to automatically scale down uploaded images to a maximum
+ * of pre-determined values or defaults. It also includes auto-rotation
+ * based on EXIF data (eg: images from digital cameras).
+ *
+ * Options:
+ * In your _config.php you may set different defaults, eg:
+ *   ScaledUploads::$max_width = 800;
+ *   ScaledUploads::$max_height = 600;
+ *   ScaledUploads::$exif_rotation = true;
+ *
+ * @license: MIT-style license http://opensource.org/licenses/MIT
+ * @author: Techno Joy development team (www.technojoy.co.nz)
+ */
+
+class ScaledUploads extends DataExtension {
+
+	public static $max_width = 960;
+	public static $max_height = 800;
+	public static $exif_rotation = true;
+
+	public function onAfterWrite() {
+		$this->ScaleUpload();
+	}
+
+	public function getMaxWidth() {
+		return self::$max_width;
+	}
+
+	public function getMaxHeight() {
+		return self::$max_height;
+	}
+
+	public function getMinSize() {
+		return (self::$max_width > self::$max_height) ? self::$max_height : self::$max_width;
+	}
+
+	public function ScaleUpload() {
+		$extension = strtolower($this->owner->getExtension());
+
+		if($this->owner->getHeight() > $this->getMinSize() || $this->owner->getWidth() > $this->getMinSize()) {
+			$original = $this->owner->getFullPath();
+			$resampled = $original . '.tmp.' . $extension;
+
+			$gd = new GD($original);
+
+			if($gd->hasGD()) {
+				/* Clone original */
+				$transformed = $gd;
+				/* If rotation allowed & JPG, test to see if orientation needs switching */
+				if (self::$exif_rotation && preg_match('/jpe?g/i', $extension)) {
+					$switchorientation = $this->exifRotation($original);
+					if ($switchorientation & $transformed) {
+						$transformed = $transformed->rotate($switchorientation);
+					}
+				}
+				/* Resize to max values */
+				if ($transformed) {
+					$transformed = $transformed->resizeRatio($this->getMaxWidth(), $this->getMaxHeight());
+				}
+				/* Overwrite original upload with resampled */
+				if ($transformed) {
+					$transformed->writeTo($resampled);
+					unlink($original);
+					rename($resampled, $original);
+				}
+			}
+		}
+	}
+
+	/*
+	 * exifRotation - return the exif rotation
+	 * @param string $FileName
+	 * @return int false|angle
+	 */
+	public function exifRotation($file) {
+		$exif = @exif_read_data($file);
+		if (!$exif) return false;
+		$ort = @$exif['IFD0']['Orientation'];
+		if (!$ort) $ort = @$exif['Orientation'];
+		switch($ort) {
+			case 3: // image upside down
+				return '180';
+			break;
+			case 6: // 90 rotate right & switch max sizes
+				return '-90';
+			break;
+			case 8: // 90 rotate left & switch max sizes
+				return '90';
+			break;
+			default:
+				return false;
+		}
+	}
+
+}
